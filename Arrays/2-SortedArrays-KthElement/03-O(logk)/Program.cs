cab451e23f111b2ad333b4f16f77146e8ceca454
Manish Mahanta <Manish.Mahanta@unisys.com>
2023-07-19 14:06:20 +0530

    Adding Divide And Conquer Approach 1 Compare the middle elements of arrays arr1 and arr2, let us call these indices mid1 and mid2 respectively. Let us assume arr1[mid1] > arr2[mid2], then clearly the elements after mid2 cannot be the required element. Set the last element of arr2 to be arr2[mid2].

---
+++ Arrays/2-SortedArrays-KthElement/03-O(logk)/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _03_O_logk_;
+
+int[] arr1 = { 5, 71, 72 };
+int[] arr2 = { 1, 4, 8, 10 };
+int k = 5;
+Console.WriteLine(KthElement.FetchKthElement(arr1, arr2, 3, 4, k));
